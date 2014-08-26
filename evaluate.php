ed22de50617cdc5537cb5c09dc4f45535bbee4c8
SudShekhar <sudshekhar02@gmail.com>
2014-08-26 22:38:22 +0530

    checking out what happens now

--- evaluate.php
+++ evaluate.php
@@ -3,10 +3,40 @@ include("include/exam.php");
 global $database;
 global $session;
 global $exam;
-for ($i=0; $i < count($_SESSION['currentqarray']); $i++) { 
-	$exam->chkans($_SESSION['currentqarray'][$i],$_POST[$_SESSION['currentqarray'][$i]])."\n";
+$totalquestions = 2;
+function debug_to_console( $data ) {
+    if ( is_array( $data ) )
+        {
+        	$output = "aray";
+        	//$output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    	}
+    else
+        $output = "<script>console.log( 'single " . $data . "' );</script>";
+    echo $output;
 }
-$exam->endexam();
-header("location: results.php?result=latest&user=".$session->username);
+
+$_SESSION['dne'] += $_SESSION['for']; 
+$id = $_SESSION['currentqarray'][$_SESSION['index']];
+$exam->chkans($id,$_POST[$id]);
+
+$_SESSION['currentqarray'][$_SESSION['index']] = -1;
+if($_SESSION['totalQuestions'] == $_SESSION['dne']){
+	$_SESSION['for'] = $_SESSION['dne'];
+	$exam->endexam();
+	$examvars = array("isWritingExam","subid","topicid","marks","corcount");
+    foreach($examvars as $toCheck){
+        if (isset($_SESSION[$toCheck]))
+          unset($_SESSION[$toCheck]);
+      }
+	header("location: results.php?result=latest&user=".$session->username);
+}
+else{
+    $_SESSION['currentqkey']=0;
+	header("location:exam.php");
+}
+
+
+
+
 
 ?>
--- exam.php
+++ exam.php
@@ -5,37 +5,31 @@ global $session;
 global $exam;
 if(!$session->logged_in)
 include("login.php");
-else if(!isset($_POST['subjectid']) && !isset($_POST['topicid']))
-include("disp.php");
-else if(isset($_POST['subjectid']) && isset($_POST['topicid']))
+else if(!isset($_POST['subjectid']) && !isset($_POST['topicid']) && !(isset($_SESSION['subid']) && isset($_SESSION['topicid']) && isset($_SESSION['marks'])))
+  include("disp.php");
+else if(isset($_POST['subjectid']) && isset($_POST['topicid']) || (isset($_SESSION['subid']) && isset($_SESSION['topicid']) && isset($_SESSION['marks'])))
 {
-$subid=$_POST['subjectid'];
-$topicid=$_POST['topicid'];
-$marks=$_POST['marks'];
-$marks = 1;
-$val=$exam->startExam($topicid,$marks);
-if($val){
-?>
-<!--force IE into Quirks Mode-->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Talent Hunt -exam</title>
-  <link rel="icon" href="favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<link rel="stylesheet" href="css/jquery.countdown.css">
-<script src="js/jquery.js"></script>
-<script src="js/jquery.countdown.js"></script>
-<script>
-$(function(){
-$('#clock').countdown({until: '+<?php echo $_SESSION['for'];?>m',format:'MS',onExpiry:comp});
-function comp(){$("#answers").submit();}
-$("#sub").click(function(){
-$.post(".php",{qid:$("#qid").val(),ans:$("input:checked").val()},function(data){$("#content").html(data);});
-});
-});
-</script>
+if(isset($_POST['subjectid']) && isset($_POST['topicid']))  {
+  $subid=$_POST['subjectid'];
+  $topicid=$_POST['topicid'];
+  $marks=1;
+  $_SESSION['subid'] = $subid;
+  $_SESSION['topicid'] = $topicid;
+  $_SESSION['marks'] = 1;
+  $val=$exam->startExam($topicid,$marks,$_POST['rounds']);
+  $_SESSION['val'] = $val;
+  $count=1;
+}
+else{
+  $subid=$_SESSION['subid'];
+  $topicid = $_SESSION['topicid'];
+  $marks=$_SESSION['marks'];
+  $val = $_SESSION['val'];
+  $count = 1;
+  $marks=1;
+}
+
+$style = <<< EOF
 <style>
 #clock{width:95px;height:40px;margin:3px 0px 0px 3px;}
 .right{float:right;padding-bottom: 5em; width: 20%;}
@@ -51,110 +45,102 @@ $.post(".php",{qid:$("#qid").val(),ans:$("input:checked").val()},function(data){
 .qs{background-color: #D08437;}
 #footer{margin-top: 20px;}
 </style>
+EOF;
+
+$tme = $_SESSION['for'];
+$script = <<< EOF
+<script>
+\$(function(){
+\$('#clock').countdown({until: '+{$tme}m',format:'MS',onExpiry:comp});
+function comp(){\$("#answers").submit();}
+\$("#sub").click(function(){
+\$.post(".php",{qid:\$("#qid").val(),ans:\$("input:checked").val()},function(data){\$("#content").html(data);});
+});
+});
+</script>
+EOF;
+$title = "Talent Hunt";
+$head = <<< EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<title>{$title}</title>
+  <link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="css/jquery.countdown.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery.countdown.js"></script>
+{$script}
+{$style}
 <meta http-equiv="content-type"
 content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet" type="text/css" href="css/footerframe.css" />
-<!--[if IE]>
-<style type="text/css">
-body
-{
-overflow-y: hidden;
-}
-div#wrapper
-{
-height: 100%;
-overflow: auto;
-}
-</style>
-<![endif]-->
 </head>
+EOF;
+$topicName = $database->gettopicname($_SESSION['top_id']);
+if($val){
+echo $head;
+
+$lft="";
+do
+{
+  $qdetails = $exam->loadmyQuestion($_SESSION['difficulty']);
+  $id = $qdetails[1];
+  $qtxt = $qdetails[0];
+  $allqids[]=$id;
+  $answers=$exam->loadans();
+  $allanswers[] = $answers;
+  $lft.="(Q".$count.") ".$qtxt."<br>";
+  $i = 'a';
+  foreach($answers as $answer)
+   {
+    $lft.= $i.")&nbsp;".$answer['value'].'<br>';
+    $i++;
+  }
+  $lft.="<br />";
+$exam->nextQ();
+$count++;
+$_SESSION['done']++;
+}
+while(!$exam->isEndOFExam());
+
+$frm = "<form action=\"evaluate.php\" method=\"post\" id=\"answers\">
+    <table>
+      <thead>
+        <td></td>
+        <td>A</td>
+        <td>B</td>
+        <td>C</td>
+        <td>D</td>
+      </thead>";
+for($i=0;$i<count($allanswers);$i++){
+  $ind = $i+1;
+  $frm.="<tr><td class=\"qs\">".$ind."</td>";
+  foreach ($allanswers[$i] as $answer) {
+     $frm.="<td><input type=\"radio\" name=".$allqids[$i]." value=".$answer['key']."></td>";
+  }
+  $frm.="</tr>";
+}
+    $frm.="</table>
+    <input type=\"submit\" value=\"go\"> 
+  </form>";
+
+$bodyup = <<< EOF
 <body>
 <div id="wrapper">
 <div id="header">
-<h1 class="logo">Talent Hunt</h1>
-<span class="strapline">Topic: <?php echo $database->gettopicname($_SESSION['top_id']);?></span>
+<h1 class="logo">{$title}</h1>
+<span class="strapline">Topic: {$topicName}</span>
 </div>
 <div id="content">
-<div class="left">	
-<?php
-$count=1;
-do
-{
-?>
-
-(Q<?php echo $count;?>)&nbsp;<?php $id=$exam->loadQuestion(); ?>
-<br>
-<?php
- $allqids[]=$id;
- $answers=$exam->loadans();
- $allanswers[] = $answers;
- 
- $i='a';
- foreach($answers as $answer)
- {
-  
-  echo $i.")&nbsp;".$answer['value'].'<br>';
-  $i++;
- }
-?>
-<br/>
-<?php 
-
-$exam->nextQ();
-$count++;
-
-
-}while(!$exam->isEndOFExam());
-
-/*(Q<?php echo $count;?>)&nbsp;<?php $id=$exam->loadQuestion(); ?>
-<br>
-<?php
- $allqids[]=$id;
- $answers=$exam->loadans();
- $allanswers[] = $answers;
- 
- $i='a';
- foreach($answers as $answer)
- {
-  
-  echo $i.")&nbsp;".$answer['value'].'<br>';
-  $i++;
- } */
-?>
-
+<div class="left"> 
+{$lft}
 </div>
  <div class="right">
-    <form action="evaluate.php" method="post" id="answers">
-    <table>
-    	<thead>
-        <td></td>
-    		<td>A</td>
-    		<td>B</td>
-    		<td>C</td>
-    		<td>D</td>
-    	</thead>
-        <?php
-     for($i=0;$i<count($allanswers);$i++){
-       
-        
-         ?>
-    	<tr>
-        <td class="qs"><?php echo $i+1;?></td>
-            <?php
-              
-                foreach ($allanswers[$i] as $answer) {
-            ?>
-    		<td><input type="radio" name="<?php echo $allqids[$i];?>" value="<?php echo $answer['key']; ?>"></td>
-            <?php
-                }
-            ?>
-    	</tr>
-    	<?php 
-    }
-    ?>
-    </table>
-    <input type="submit" value="go"> 
-  </form>
+{$frm}
  </div>
 </div>
 </div>
@@ -163,11 +149,12 @@ $count++;
 </div>
 </body>
 </html>
-<?php
+EOF;
+echo $bodyup;
 }
 else
 { 
-echo "there are not as many questions as you asked";
+echo "Sorry, there are not as many questions as you asked for";
 }
 }
 ?>
